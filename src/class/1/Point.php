cc3bf4ce3da3bbae93ee7e9e6d0c205b9f47bc92
Filipp <ffilips@bk.ru>
2024-01-09 13:06:05 +0300

    Update src/class/1

--- src/class/1/Point.php
+++ src/class/1/Point.php
@@ -3,4 +3,9 @@
 class Point {
     public $x;
     public $y;
+
+    function __construct($x, $y){
+        $this->x = $x;
+        $this->y = $y;
+    }
 }
--- src/class/1/PointFunctions.php
+++ src/class/1/PointFunctions.php
@@ -4,18 +4,13 @@ require_once 'Point.php';
 
 function getMidpoint($point1, $point2): Point
 {
-    $midpoint = new Point();
-    $midpoint->x = ($point1->x + $point2->x) / 2;
-    $midpoint->y = ($point1->y + $point2->y) / 2;
-    return $midpoint;
+    $x = ($point1->x + $point2->x) / 2;
+    $y = ($point1->y + $point2->y) / 2;
+    return new Point($x, $y);
 }
 
-$point1 = new Point();
-$point1->x = 1;
-$point1->y = 10;
-$point2 = new Point();
-$point2->x = 10;
-$point2->y = 1;
+$point1 = new Point(1, 10);
+$point2 = new Point(10, 1);
 
 $midpoint = getMidpoint($point1, $point2);
 echo $midpoint->x; // 5.5
